5ffb19d7f877f952a5f4dfca4fff78e51f1037dc
Oknanashi <oknanashi@gmail.com>
2019-03-25 19:31:05 +0300

    'test'

--- index.js
+++ index.js
@@ -24,14 +24,11 @@ mongoose
 app.use('/api/users',users)
 app.use('/api/comments',comments)
 
-//Production test
-if(process.env.NODE_ENV==='production'){
-  //Set static
-  app.use(express.static('black-moviesearcher/build'))
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'black-moviesearcher','build','index.html'))
-  })
-}
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname,'build')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
 
 const port = process.env.PORT || 5000
 app.listen(port,()=>console.log(`Server running on port ${port}`))
